395ecdfdb955dd966da8f7c9f6eae8d2b481e42d
albernet <andrea042280@aol.com>
2018-02-03 15:44:31 -0500

    remove statement content in test.js, replaced with assign,ent 8 Break and Continue to display (var i number) 0,1,2,3,4,5,6,7 and then read I have broken out of the loop

--- test.js
+++ test.js
@@ -1,9 +1,19 @@
-var links = document.getElementsByTagName("a");
+for (i = 0; i < 10; i++){
+	
+	if(i === 5 || i ===3){
+		
+		continue;
+		
+	}
+	
+	console.log(i);
+	
+	if(i === 7){
+		
+		break;
+		
+	}
 
-for (i = 1; i <= links.length; i++){
-	
-	console.log("this is the link number" + i);
-	
 }
 
-document.write("all links now looped");
+ console.log("I have broken out of the loop");
