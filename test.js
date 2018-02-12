d89cdc542a25d512c431a6bcb5e78b2441a0d5b9
albernet <andrea042280@aol.com>
2018-02-12 12:40:39 -0500

    correction, Assignment 010 this is the correct function for average of 5 numbers

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
-function getAverage (a,b){
+function getAverage (a,b,c,d,e,){
 	
-	var average = (a + b) / 5;
+	var average = (a + b + c + d + e) / 5;
 	console.log(average);
 	return average;
 }
 
 var myResult = getAverage(7,8,9,10,11);
 console.log("the average is" + myResult);
+
+
+
+
